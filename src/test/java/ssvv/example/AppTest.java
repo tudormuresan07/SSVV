fb2cb82a41afe80dd7ed04c0cc54059a65c9072f
tudormuresan07 <tudorvladmuresan@gmail.com>
2022-03-22 13:32:11 +0200

    First Commit

---
+++ src/test/java/ssvv/example/AppTest.java
@@ -0,0 +1,70 @@
+package ssvv.example;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import domain.Nota;
+import domain.Student;
+import domain.Tema;
+import org.junit.Test;
+import repository.NotaXMLRepository;
+import repository.StudentXMLRepository;
+import repository.TemaXMLRepository;
+import service.Service;
+import validation.NotaValidator;
+import validation.StudentValidator;
+import validation.TemaValidator;
+import validation.Validator;
+
+import java.util.Iterator;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+    @Test
+    public void test__saveStudent__addValidStudent__studentCorrectlyAdded(){
+        Validator<Student> studentValidator = new StudentValidator();
+        Validator<Tema> temaValidator = new TemaValidator();
+        Validator<Nota> notaValidator = new NotaValidator();
+
+        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti_test.xml");
+        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
+        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
+
+        Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
+
+        service.saveStudent("1","Gion",935);
+        int nrOfStudents=0;
+        for (Student student : service.findAllStudents()) {
+            nrOfStudents += 1;
+        }
+        assertEquals(1, nrOfStudents);
+    }
+
+    @Test
+    public void test__saveStudent__addInvalidStudent__studentNotAdded(){
+        Validator<Student> studentValidator = new StudentValidator();
+        Validator<Tema> temaValidator = new TemaValidator();
+        Validator<Nota> notaValidator = new NotaValidator();
+
+        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti_test.xml");
+        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
+        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
+
+        Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
+
+        int result=service.saveStudent("1","Gion",935);
+        assertEquals(0, result);
+    }
+}
